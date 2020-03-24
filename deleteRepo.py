5b05aaf32bfbff5e682b760bba993a653b6c2bab
Anchitaa25 <Anchitaa.25@gmail.com>
2020-03-25 00:27:11 +0530

    Update deleteRepo.py

--- deleteRepo.py
+++ deleteRepo.py
@@ -6,6 +6,10 @@ Project_Name = sys.argv[3]
 Repository_Name = sys.argv[1]
 User_Name = sys.argv[4]
 Personal_Token = sys.argv[5]
+param1 = sys.argv[6]
+param2 = sys.argv[7]
+def add():
+  result = param1 + param2
 def deleteRepository():
   get_URL = 'https://dev.azure.com/'+Organization_Name+'/'+Project_Name+'/_apis/git/repositories/'+Repository_Name+'?api-version=5.1'
   get_ID_request = requests.get(url = get_URL , auth = (User_Name,Personal_Token))
@@ -16,6 +20,7 @@ def deleteRepository():
 
 try:
   deleteRepository()
+  add()
 except KeyError:
   print("URL Incorrect. Check values for Repository/Organization/Project Name")
 except ValueError:
